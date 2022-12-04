516c4fd91af0e8036a428b6c754b1467a0121979
YurijCroz <valetkrest@gmail.com>
2022-12-04 17:31:07 +0200

    refactoring: delete src/router/, add src/router.js, src/routes/authRouter.js|chatRouter.js|contestRouter.js|payRouter.js|userRouter.js

---
+++ server/src/routes/payRouter.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const basicMiddlewares = require("../middlewares/basicMiddlewares");
+const userController = require("../controllers/userController");
+const checkToken = require("../middlewares/checkToken");
+const validators = require("../middlewares/validators");
+const upload = require("../utils/fileUpload");
+
+const payRouter = Router();
+
+payRouter.post(
+  "/pay",
+  checkToken.checkToken,
+  basicMiddlewares.onlyForCustomer,
+  upload.uploadContestFiles,
+  basicMiddlewares.parseBody,
+  validators.validateContestCreation,
+  userController.payment
+);
+
+payRouter.post(
+  "/cashout",
+  checkToken.checkToken,
+  basicMiddlewares.onlyForCreative,
+  userController.cashout
+);
