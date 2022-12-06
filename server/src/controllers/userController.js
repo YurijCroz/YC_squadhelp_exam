33e5237a84e4a56f6b5d63869b14a3cdaa4bc2b2
YurijCroz <valetkrest@gmail.com>
2022-12-06 14:59:11 +0200

    fix: controllers/ files. create contest\complete it's working=)

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,6 +1,12 @@
 const jwt = require('jsonwebtoken');
 const CONSTANTS = require('../constants');
-const bd = require('../models');
+const {
+  Sequelize,
+  sequelize,
+  Contest,
+  Offer,
+  Rating
+} = require('../models');
 const NotUniqueEmail = require('../errors/NotUniqueEmail');
 const moment = require('moment');
 const { v4: uuid } = require('uuid');
@@ -74,14 +80,14 @@ module.exports.changeMark = async (req, res, next) => {
   const { isFirst, offerId, mark, creatorId } = req.body;
   const userId = req.tokenData.userId;
   try {
-    transaction = await bd.sequelize.transaction(
-      { isolationLevel: bd.Sequelize.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED });
+    transaction = await sequelize.transaction(
+      { isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED });
     const query = getQuery(offerId, userId, mark, isFirst, transaction);
     await query();
-    const offersArray = await bd.Ratings.findAll({
+    const offersArray = await Rating.findAll({
       include: [
         {
-          model: bd.Offers,
+          model: Offer,
           required: true,
           where: { userId: creatorId },
         },
@@ -106,9 +112,9 @@ module.exports.changeMark = async (req, res, next) => {
 module.exports.payment = async (req, res, next) => {
   let transaction;
   try {
-    transaction = await bd.sequelize.transaction();
+    transaction = await sequelize.transaction();
     await bankQueries.updateBankBalance({
-      balance: bd.sequelize.literal(`
+      balance: sequelize.literal(`
                 CASE
             WHEN "cardNumber"='${ req.body.number.replace(/ /g,
     '') }' AND "cvc"='${ req.body.cvc }' AND "expiry"='${ req.body.expiry }'
@@ -119,7 +125,7 @@ module.exports.payment = async (req, res, next) => {
     },
     {
       cardNumber: {
-        [ bd.Sequelize.Op.in ]: [
+        [ Sequelize.Op.in ]: [
           CONSTANTS.SQUADHELP_BANK_NUMBER,
           req.body.number.replace(/ /g, ''),
         ],
@@ -140,7 +146,7 @@ module.exports.payment = async (req, res, next) => {
         prize,
       });
     });
-    await bd.Contests.bulkCreate(req.body.contests, transaction);
+    await Contest.bulkCreate(req.body.contests, transaction);
     transaction.commit();
     res.send();
   } catch (err) {
@@ -174,12 +180,12 @@ module.exports.updateUser = async (req, res, next) => {
 module.exports.cashout = async (req, res, next) => {
   let transaction;
   try {
-    transaction = await bd.sequelize.transaction();
+    transaction = await sequelize.transaction();
     const updatedUser = await userQueries.updateUser(
-      { balance: bd.sequelize.literal('balance - ' + req.body.sum) },
+      { balance: sequelize.literal('balance - ' + req.body.sum) },
       req.tokenData.userId, transaction);
     await bankQueries.updateBankBalance({
-      balance: bd.sequelize.literal(`CASE 
+      balance: sequelize.literal(`CASE 
                 WHEN "cardNumber"='${ req.body.number.replace(/ /g,
     '') }' AND "expiry"='${ req.body.expiry }' AND "cvc"='${ req.body.cvc }'
                     THEN "balance"+${ req.body.sum }
@@ -190,7 +196,7 @@ module.exports.cashout = async (req, res, next) => {
     },
     {
       cardNumber: {
-        [ bd.Sequelize.Op.in ]: [
+        [ Sequelize.Op.in ]: [
           CONSTANTS.SQUADHELP_BANK_NUMBER,
           req.body.number.replace(/ /g, ''),
         ],
--- server/src/dbMongo/mongoose.js
+++ server/src/dbMongo/mongoose.js
@@ -3,10 +3,10 @@ const path = require('path');
 const env = process.env.NODE_ENV || 'development';
 const configPath = path.join(__dirname, '..', 'config/mongoConfig.json');
 const config = require(configPath)[ env ];
-console.log('================>',config)
+
 mongoose.connect(
   `mongodb://${ config.host }:${ config.port }/${ config.database }`,
-  /* { useNewUrlParser: true, useUnifiedTopology: true }, */ (err) => {
+  { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) {
       console.log('ERoRR=>',err);
       process.exit(1);
