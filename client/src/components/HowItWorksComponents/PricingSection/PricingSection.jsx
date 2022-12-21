44804ef170653f5d61be4f1880f0e5e95b982e0f
YurijCroz <valetkrest@gmail.com>
2022-12-21 19:41:25 +0200

    page HowItWorks finished

---
+++ client/src/components/HowItWorksComponents/PricingSection/PricingSection.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import CONSTANTS from "../../../constants";
+import styles from "./PricingSection.module.sass";
+
+function PricingSection() {
+  return (
+    <section className={styles.container}>
+      <section className={styles.firstContainer}>
+        <article className={styles.infoBlock}>
+          <ul className={styles.listUnstyled}>
+            <li className={styles.media}>
+              <span className={styles.btnIcon}>
+                <span className="fas fa-angle-right"></span>
+              </span>
+              <div className={styles.media__body}>
+                <h5>Pay a Fraction of cost vs hiring an agency</h5>
+                <p>
+                  For as low as $199, our naming contests and marketplace allow
+                  you to get an amazing brand quickly and affordably.
+                </p>
+              </div>
+            </li>
+            <li className={styles.border}></li>
+            <li className={styles.media}>
+              <span className={styles.btnIcon}>
+                <span className="fas fa-angle-right"></span>
+              </span>
+              <div className={styles.media__body}>
+                <h4>Satisfaction Guarantee</h4>
+                <p>
+                  Of course! We have policies in place to ensure that you are
+                  satisfied with your experience. <a href="#">Learn more</a>
+                </p>
+              </div>
+            </li>
+          </ul>
+        </article>
+      </section>
+      <section className={styles.secondContainer}>
+        <article className={styles.infoBlock} >
+          <ul className={styles.listUnstyled} >
+            <li className={styles.media} >
+              <div className={styles.media__body} >
+                <h3>Questions?</h3>
+                <p className={styles.whiteText} >Speak with a Squadhelp platform expert to learn more and get your questions answered.</p>
+                <button className={styles.whiteBtn} >Schedule Consultation</button>
+                <a href={`tel:${CONSTANTS.CONTACTS.TEL}`} >
+                  <img src={`${CONSTANTS.STATIC_IMAGES_PATH}phone_icon.svg`} alt="phone-call" />
+                  {CONSTANTS.CONTACTS.TEL}
+                </a>
+                <span>Call us for assistance</span>
+              </div>
+            </li>
+          </ul>
+        </article>
+      </section>
+    </section>
+  );
+}
+
+export default PricingSection;
--- client/src/pages/HowItWorks/HowItWorks.jsx
+++ client/src/pages/HowItWorks/HowItWorks.jsx
@@ -8,6 +8,8 @@ import OurServices from "../../components/HowItWorksComponents/OurServices/OurSe
 import StepsContest from "../../components/HowItWorksComponents/StepsContest/StepsContest";
 import FaqContest from "../../components/HowItWorksComponents/FaqContest/FaqContest";
 import StatsSection from "../../components/HowItWorksComponents/StatsSection/StatsSection.jsx";
+import PricingSection from "../../components/HowItWorksComponents/PricingSection/PricingSection";
+import SatisfactionCard from "../../components/HowItWorksComponents/SatisfactionCard/SatisfactionCard";
 import classnames from "classnames";
 
 function HowItWorks() {
@@ -32,7 +34,10 @@ function HowItWorks() {
         <section className={styles.ctaSection}>
           <article className={styles.ctaContainer}>
             <h3>Ready to get started?</h3>
-            <p>Fill out your contest brief and begin receiving custom name suggestions within minutes.</p>
+            <p>
+              Fill out your contest brief and begin receiving custom name
+              suggestions within minutes.
+            </p>
             <a href="#">Start A Contest</a>
           </article>
           <div className={classnames(styles.svgImg, styles.svgFirst)}>
@@ -48,8 +53,55 @@ function HowItWorks() {
             />
           </div>
         </section>
-        <section className={styles.stats} >
+        <section className={styles.stats}>
           <StatsSection />
+        </section>
+        <section className={styles.pricing}>
+          <PricingSection />
+        </section>
+        <section className={styles.satisfactionGaurenteedModal}>
+          <SatisfactionCard />
+        </section>
+        <section className={styles.clients}>
+          <section className={styles.clients__container}>
+            <section className={styles.title}>
+              <article className={styles.title__block}>
+                <h6>Featured In</h6>
+              </article>
+            </section>
+            <section className={styles.clients__section}>
+              <section className={styles.clients__body}>
+                <section className={styles.client}>
+                  <div className={styles.client__logo}>
+                    <a href="#">
+                      <img src={`${CONSTANTS.STATIC_IMAGES_PATH}forbes.svg`} alt="forbes" />
+                    </a>
+                  </div>
+                </section>
+                <section className={styles.client}>
+                  <div className={styles.client__logo}>
+                    <a href="#">
+                      <img src={`${CONSTANTS.STATIC_IMAGES_PATH}TNW.svg`} alt="TNW" />
+                    </a>
+                  </div>
+                </section>
+                <section className={styles.client}>
+                  <div className={styles.client__logo}>
+                    <a href="#">
+                      <img src={`${CONSTANTS.STATIC_IMAGES_PATH}chicago.svg`} alt="chicago" />
+                    </a>
+                  </div>
+                </section>
+                <section className={styles.client}>
+                  <div className={styles.client__logo}>
+                    <a href="#">
+                      <img src={`${CONSTANTS.STATIC_IMAGES_PATH}Mashable.svg`} alt="Mashable" />
+                    </a>
+                  </div>
+                </section>
+              </section>
+            </section>
+          </section>
         </section>
       </main>
       <Footer />
