fdc5b7ebddeff3785ea1f84d78c82572028aea63
YurijCroz <valetkrest@gmail.com>
2023-02-08 19:51:20 +0200

    crete logger

---
+++ server/src/log.js
@@ -0,0 +1,35 @@
+"use strict";
+const winston = require("winston");
+const { createLogger, format, transports } = require("winston");
+const { combine, timestamp, printf } = format;
+
+const myFormat = printf(({ message, timestamp, code, stack }) => {
+  return JSON.stringify({
+    message,
+    time: timestamp,
+    code,
+    stackTrace: { stack },
+  });
+});
+
+const logger = createLogger({
+  level: "info",
+  format: combine(timestamp(), myFormat),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new transports.File({
+      filename: `${(__dirname, "./")}/log/error.log`,
+      level: "error",
+    }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new transports.Console({
+      format: format.colorize(),
+    })
+  );
+}
+
+module.exports = logger;
