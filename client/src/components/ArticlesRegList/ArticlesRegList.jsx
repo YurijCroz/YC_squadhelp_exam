85d6122112da1cb2136107104b252a35fec30e57
YurijCroz <valetkrest@gmail.com>
2023-04-03 18:37:44 +0300

    rework: ArticlesRegList.jsx

--- client/src/components/ArticlesRegList/ArticlesRegList.jsx
+++ client/src/components/ArticlesRegList/ArticlesRegList.jsx
@@ -1,46 +1,44 @@
-import articles from "./articles.json";
 import React from "react";
+import articles from "./articles.json";
 import styles from "./ArticlesRegList.module.sass";
 import CONSTANTS from "../../constants";
 
+function AdditionalArticle() {
+  return (
+    <article>
+      <h3 className={styles.headerArticle}>
+        I have other questions! How can I get in touch with Squadhelp?
+      </h3>
+      <p className={styles.article}>
+        Check out our <span className={styles.orangeSpan}>FAQs</span> or send us
+        a <span className={styles.orangeSpan}>message</span>. For assistance
+        with launching a contest, you can also call us at{" "}
+        {
+          <a
+            className={styles.orangeSpan}
+            href={`tel:${CONSTANTS.CONTACTS.TEL}`}
+          >
+            {CONSTANTS.CONTACTS.TEL}
+          </a>
+        }{" "}
+        or schedule a <span className={styles.orangeSpan}>Branding Consultation</span>
+      </p>
+    </article>
+  );
+}
+
 function ArticlesRegList() {
-  const getAdditionalArticle = (index) => {
-    if (index === 1) {
-      return (
-        <article>
-          <h3 className={styles.headerArticle}>
-            I have other questions! How can I get in touch with Squadhelp?
-          </h3>
-          <p className={styles.article}>
-            Check out our <span className={styles.orangeSpan}>FAQs</span> or
-            send us a <span className={styles.orangeSpan}>message</span>. For
-            assistance with launching a contest, you can also call us at{" "}
-            {
-              <a
-                className={styles.orangeSpan}
-                href={`tel:${CONSTANTS.CONTACTS.TEL}`}
-              >
-                {CONSTANTS.CONTACTS.TEL}
-              </a>
-            }{" "}
-            or schedule a{" "}
-            <span className={styles.orangeSpan}>Branding Consultation</span>
-          </p>
-        </article>
-      );
-    }
-  };
   return (
     <section className={styles.articlesMainContainer}>
-      {articles.map((column, index) => (
-        <section className={styles.columnContainer} key={index}>
-          {column.map((article, index) => (
-            <article key={index}>
+      {articles.map((column, columnIndex) => (
+        <section className={styles.columnContainer} key={columnIndex}>
+          {column.map((article, articleIndex) => (
+            <article key={articleIndex}>
               <h3 className={styles.headerArticle}>{article.title}</h3>
               <p className={styles.article}>{article.text}</p>
             </article>
           ))}
-          {getAdditionalArticle(index)}
+          {columnIndex === 1 && <AdditionalArticle />}
         </section>
       ))}
     </section>
