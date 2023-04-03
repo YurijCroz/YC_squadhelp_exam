3b6e3f4deb35e69b7f58a9a2e0b1236102ef0f83
YurijCroz <valetkrest@gmail.com>
2023-04-03 18:38:50 +0300

    rework: BackButton.js

--- client/src/components/BackButton/BackButton.js
+++ client/src/components/BackButton/BackButton.js
@@ -2,10 +2,10 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import styles from "./BackButton.module.sass";
 
-const BackButton = (props) => {
-  function clickHandler() {
-    props.history.goBack();
-  }
+const BackButton = ({ history }) => {
+  const clickHandler = () => {
+    history.goBack();
+  };
 
   return (
     <button onClick={clickHandler} className={styles.buttonContainer}>
