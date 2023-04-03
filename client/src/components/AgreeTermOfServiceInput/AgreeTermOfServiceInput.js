a8793638683055793e8b40aa017dbde808a962bf
YurijCroz <valetkrest@gmail.com>
2023-04-03 18:36:54 +0300

    rework: AgreeTermOfServiceInput.js

--- client/src/components/AgreeTermOfServiceInput/AgreeTermOfServiceInput.js
+++ client/src/components/AgreeTermOfServiceInput/AgreeTermOfServiceInput.js
@@ -3,27 +3,20 @@ import { Field } from "formik";
 
 const AgreeTermOfServiceInput = ({ id, type, classes, label, ...rest }) => (
   <Field {...rest}>
-    {(props) => {
-      const {
-        meta: { touched, error },
-        field,
-      } = props;
-
-      return (
-        <>
-          <section className={classes.container}>
-            <input {...field} placeholder={label} id={id} type={type} />
-            <label htmlFor={id}>
-              By clicking this checkbox, you agree to our{" "}
-              <a href="https://www.google.com" target="_blank" rel="noreferrer">
-                Terms of Service.
-              </a>
-            </label>
-          </section>
-          {touched && error && <span className={classes.warning}>{error}</span>}
-        </>
-      );
-    }}
+    {({ meta: { touched, error }, field }) => (
+      <>
+        <section className={classes.container}>
+          <input {...field} placeholder={label} id={id} type={type} />
+          <label htmlFor={id}>
+            By clicking this checkbox, you agree to our{" "}
+            <a href="https://www.google.com" target="_blank" rel="noreferrer">
+              Terms of Service.
+            </a>
+          </label>
+        </section>
+        {touched && error && <span className={classes.warning}>{error}</span>}
+      </>
+    )}
   </Field>
 );
 
