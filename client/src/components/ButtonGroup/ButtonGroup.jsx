62d16ab22a3e9a6f7ceb6599b62802425e75df91
YurijCroz <valetkrest@gmail.com>
2023-04-03 18:39:55 +0300

    rework: ButtonGroup.jsx

--- client/src/components/ButtonGroup/ButtonGroup.jsx
+++ client/src/components/ButtonGroup/ButtonGroup.jsx
@@ -1,39 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./ButtonGroup.module.sass";
-import classnames from "classnames";
 
 function ButtonGroup() {
-  const [valueInput, setValueInput] = useState("yes");
+  const [valueInput, setValueInput] = useState(null);
+  const activeRef = useRef(null);
 
-  const activeHandler = ({ target }) => {
-    if (target.localName !== "div") target = target.offsetParent;
-    setValueInput(target.dataset.choose);
-    removeActiveStyle();
-    addActiveStyle(target);
+  useEffect(() => {
+    activeRef.current.classList.add(styles.active);
+    setValueInput(activeRef.current.dataset.choose);
+  }, []);
+
+  const handleClick = (event) => {
+    const target = event.target.dataset.choose
+      ? event.target
+      : event.target.offsetParent;
+    const choose = target.dataset.choose;
+
+    if (choose) {
+      setValueInput(choose);
+      activeRef.current.classList.remove(styles.active);
+      target.classList.add(styles.active);
+      activeRef.current = target;
+    }
   };
-  const removeActiveStyle = () =>
-    document
-      .querySelector(`div.${styles.active}`)
-      .classList.remove(styles.active);
-
-  const addActiveStyle = (target) => target.classList.add(styles.active);
 
   return (
     <section className={styles.container}>
       <input type="hidden" name="company_url_needed" value={valueInput} />
-      <div data-choose="asname" onClick={activeHandler} className={styles.card}>
+      <div data-choose="asname" onClick={handleClick} className={styles.card}>
         <span className={styles.badge}>Yes</span>
         <h5>The Domain should exactly match the name</h5>
       </div>
       <div
         data-choose="yes"
-        onClick={activeHandler}
-        className={classnames([styles.card, styles.active])}
+        onClick={handleClick}
+        className={styles.card}
+        ref={activeRef}
       >
         <span className={styles.badge}>Yes</span>
         <h5>But minor variations are allowed (Recommended)</h5>
       </div>
-      <div data-choose="no" onClick={activeHandler} className={styles.card}>
+      <div data-choose="no" onClick={handleClick} className={styles.card}>
         <span className={styles.badge}>No</span>
         <h5>I am only looking for a name, not a Domain</h5>
       </div>
